e3567a01b3dfda29fda79ea167e84f501c019006
cwilson-su <125030769+cwilson-su@users.noreply.github.com>
2026-03-22 09:13:14 +0100

    set the path for chord implementation

---
+++ src/dht/src/chord/Main.java
@@ -0,0 +1,21 @@
+package chord;
+
+import dht.Address;
+
+public class Main {
+    public static void main(String[] args) {
+    	if (args.length < 1) {
+            System.out.println("Usage: java dht.Main <port> [peerIP:peerPort...]");
+            return;
+        }
+        
+        int port = Integer.parseInt(args[0]);
+        Address[] peers = new Address[args.length - 1];
+        for (int i = 1; i < args.length; i++) {
+            peers[i - 1] = Address.parse(args[i]);
+        }
+
+        ChordNode n = new ChordNode(port, peers);
+        n.start();
+    }
+}
